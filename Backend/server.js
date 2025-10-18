2a4631ca86a8515d3243a67b2d6ea2ba06adac46
shika <shika5027it@gmail.com>
2025-10-18 16:26:44 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,9 @@
 import  express from 'express';
+import dotenv from 'dotenv';
+
+
+
+
 
 import mongoose from 'mongoose'; 
 import cors from 'cors'   
@@ -7,7 +12,7 @@ import bodyParser from 'body-parser';
 import postRouter from './router/posts.js';
 
 const app=express();
- 
+ dotenv.config();
 const PORT=process.env.PORT || 8000;   // this take port from env file or 5000
 
 app.use(bodyParser.json());  // it will parse json data
@@ -17,7 +22,10 @@ app.use(cors())
 
 // mongodb connection       
 
-mongoose.connect('mongodb://localhost:27017/e-commerce')
+mongoose.connect(process.env.MONGO_URI ||'mongodb://localhost:27017/e-commerce', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(()=>console.log("Mongo DB connected"))
 .catch((err)=>console.log('DB err',err));
 
